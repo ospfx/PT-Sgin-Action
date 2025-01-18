ecf034072a71240de7ff0456c8f422a37b027d06
Qianyue <admin@ospf.us.kg>
2025-01-18 18:25:40 +0800

    Update pt_sgin.py

--- pt_sgin.py
+++ pt_sgin.py
@@ -95,7 +95,7 @@ if __name__ == "__main__":
     sites = parse_sites_from_env()
     for site in sites:
         check_in(site['url'], site.get('name', 'Unknown'), site['cookie'])
-        if _token and chat_id:
+        if telegram_bot_token and chat_id:
           telegram_Bot(telegram_bot_token, chat_id, thread_id, result)
         if pushplus_token:
           pushplus_ts(pushplus_token, result)
